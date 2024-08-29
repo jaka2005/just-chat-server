2dd43855da09b6eac248a0f7ad6325e0f472faad
jaka2005 <jakalogin1808@gmail.com>
2024-08-29 05:51:03 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "2.0.0"
+}
+
+group = "funn.j2k"
+version = "0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val ktor_version = "2.3.12"
+    implementation("io.ktor:ktor-network:$ktor_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC.2")
+    testImplementation(kotlin("test"))
+    implementation(kotlin("reflect"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+package funn.j2k
+
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
+import io.ktor.utils.io.core.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val selectorManager = SelectorManager(Dispatchers.IO)
+    val serverSocket = aSocket(selectorManager).tcp().bind("127.0.0.1", 9002)
+
+    while (true) {
+        val socket = serverSocket.accept()
+
+        launch {
+            val receiveChannel = socket.openReadChannel()
+            val sendChannel = socket.openWriteChannel(autoFlush = true)
+
+            try {
+                while (true) {
+                    val event = Event.getEventReaderByCode(
+                        receiveChannel.readByte().toInt()
+                    ).readFullEvent(receiveChannel)
+                    when (event) {
+                        is Accept -> {}
+                        is Message -> {
+                            println(event)
+                            sendChannel.writePacket(ByteReadPacket(event.text.encodeToByteArray()))
+                        }
+                    }
+                }
+            } catch (e: Throwable) {
+                println(e.message)
+                socket.close()
+            }
+        }
+    }
+}
